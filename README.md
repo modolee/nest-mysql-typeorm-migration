5f66ef09769550a6a9f34347c6169cc4c883b308
modolee <modolee.si@gmail.com>
2023-03-01 13:07:52 +0900

    feat: MySQL, Nest.js 컨테이너 세팅

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# nest-mysql-typeorm-migration
+# Migration (Seeding) Example using TypeORM for MySQL with Nest.js
+
+- Nest.js, MySQL을 이용해서 프로젝트를 진행할 때, TypeORM을 사용해서 개발하는 경우가 많다.
+- TypeORM의 migration 기능을 이용해서 초기 데이터 (Seed Data)를 생성해 보자.
---
+++ start.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+CMD="docker compose \
+  -p seeding-tutorial \
+  -f docker/mysql/docker-compose.yml \
+  -f docker/api/docker-compose.yml"
+
+function build()
+{
+  echo "##### Docker Compose Build #####"
+  eval "$CMD" --profile "$1" --env-file "$2" build
+}
+
+function run()
+{
+  echo "##### Docker Compose Run #####"
+  eval "$CMD" --profile "$1" --env-file "$2" up --remove-orphans
+}
+
+function menu()
+{
+  PS3="빌드와 실행 방식을 선택하세요 > "
+  COLUMNS=30
+  options=("Build and Run" "Run" "Quit")
+  select yn in "${options[@]}"; do
+      case $yn in
+          "Build and Run" ) build "$1" "$2"; run "$1" "$2"; break;;
+          "Run" )           run   "$1" "$2"; break;;
+          "Quit" )          exit;;
+      esac
+  done
+}
+
+menu "all" ".env"
